15872bd3fba3e5cf74c5ba74e86828881f5c668b
David Arayan <david.arayan12@gmail.com>
2018-08-10 14:12:32 +1000

    - Purge compiled objects

--- delaunay_wasm_build/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Set our EMSDK Environment Variables
-#source ./../../../emscripten/emsdk/emsdk_env.sh
-
-# run a super-simple HTML server
-#python -m SimpleHTTPServer 8080
-python ../simple_server.py
-#emrun dct.html --browser chrome
