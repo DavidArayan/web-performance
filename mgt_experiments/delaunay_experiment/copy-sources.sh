bfa9db2f9fc4322c9291066a43cb86545507c454
David Arayan <david.arayan12@gmail.com>
2019-02-14 21:34:57 +1100

    - Modified the delaunay experiment to fit the new compile structure - Modified some of the Makefiles to make things more consistent

--- mgt_experiments/delaunay_experiment/copy-sources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# copy all the sources from the original folder
-# to the destination (target) folder
-find "delaunay_original" -name \*.c -exec cp {} "delaunay_asm" \;
-find "delaunay_original" -name \*.h -exec cp {} "delaunay_asm" \;
-
-find "delaunay_original" -name \*.c -exec cp {} "delaunay_wasm" \;
-find "delaunay_original" -name \*.h -exec cp {} "delaunay_wasm" \;
--- mgt_experiments/delaunay_experiment/runner.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python ../../simple_server/simple_server.py
