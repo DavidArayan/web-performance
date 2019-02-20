4756d3761504bd2af80ea3cd54cde80f6bf2892f
David Arayan <david.arayan12@gmail.com>
2019-02-20 20:22:12 +1100

    - Added a helper mode to sserver_up to ensure it's always using the newest compiled files - Expanded on the descriptions for the Matrix and Delaunay Triangulator experiments

--- sserver_up.sh
+++ sserver_up.sh
@@ -2,5 +2,14 @@
 
 echo "Running sserver Docker Image"
 
+# spin our container up
 docker run -p 8080:8080 -dit --name sserver sserver:latest
+
+# delete our old public folder inside the container
+docker exec -ti sserver sh -c "rm -rf /usr/src/app/sserver/public"
+
+# copy our current local version into the container
+docker cp sserver/public sserver:/usr/src/app/sserver/public
+
+# run our http-server to start serving the files on localhost
 docker exec -ti sserver sh -c "http-server ./sserver/public"
