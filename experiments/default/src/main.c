3829362d045c47f7fe030f009f9b3824559ba2c5
David Arayan <david.arayan12@gmail.com>
2019-02-21 15:20:29 +1100

    - Separated the logic between the web_main and normal main functions - Ensure that the WASM source is compiled with a js output only

--- experiments/default/src/main.c
+++ experiments/default/src/main.c
@@ -16,9 +16,23 @@ void say_hello() {
 	printf("Hello World!\n");
 }
 
-int main(int argc, char ** argv) {
+#ifdef __EMSCRIPTEN__
+EMSCRIPTEN_KEEPALIVE
+#endif
+int main_web(int argc, char **argv) {
 	say_hello();
+
+	return 0;
 }
+
+/**
+ * Main is only compiled for non web targets
+ */
+#ifndef __EMSCRIPTEN__
+int main(int argc, char **argv) {
+	return main_web(argc, argv);
+}
+#endif
 
 #ifdef __cplusplus
 }
