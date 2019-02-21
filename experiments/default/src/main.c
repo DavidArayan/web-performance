493d0f630ea1ac04ffd7ca05356ee206c45d9503
David Arayan <david.arayan12@gmail.com>
2019-02-21 15:31:11 +1100

    - remove the web main, we will use the standard main moving forwards

--- experiments/default/src/main.c
+++ experiments/default/src/main.c
@@ -16,23 +16,10 @@ void say_hello() {
 	printf("Hello World!\n");
 }
 
-#ifdef __EMSCRIPTEN__
-EMSCRIPTEN_KEEPALIVE
-#endif
-int main_web(int argc, char **argv) {
+int main(int argc, char **argv) {
 	say_hello();
-
 	return 0;
 }
-
-/**
- * Main is only compiled for non web targets
- */
-#ifndef __EMSCRIPTEN__
-int main(int argc, char **argv) {
-	return main_web(argc, argv);
-}
-#endif
 
 #ifdef __cplusplus
 }
