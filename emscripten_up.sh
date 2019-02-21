26979510202726df908a9496a26760f30114859b
David Arayan <david.arayan12@gmail.com>
2019-02-22 10:34:14 +1100

    - Further code comment additions

--- emscripten_up.sh
+++ emscripten_up.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo "Running emscripten Docker Image"
+echo "Running emscripten Docker Image - This will compile all our source files"
 
 docker run -dit --name emscripten emscripten:latest
 
@@ -10,10 +10,13 @@ docker exec -ti emscripten sh -c "rm -rf /usr/src/app/experiments"
 # copy all our source files into the container
 docker cp experiments emscripten:/usr/src/app/experiments
 
+# execute out ruby compile script which will compile all of our source files
 docker exec -ti emscripten sh -c "cd experiments && ruby compile.rb"
 
+# purge our previously compiled sources from the server directory
 rm -rf sserver/public/compiled
 
+# copy all compiled sources from within our container into the server directory
 docker cp emscripten:/usr/src/app/experiments/compiled sserver/public
 
 # once all done, stop the container
