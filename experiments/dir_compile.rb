a0a6f8db5426b01510087e9adebb9be3f45d2f81
David Arayan <david.arayan12@gmail.com>
2019-02-21 16:21:50 +1100

    - Simplify the dir_compile.rb script - Added the index_asm and index_wasm html codes which look nicer

--- experiments/dir_compile.rb
+++ experiments/dir_compile.rb
@@ -4,31 +4,20 @@ require 'fileutils'
 
 cdir = ARGV[0]
 
-puts " ----------------------------"
-puts " --- Running ASM Makefile ---"
-puts " ----------------------------"
-
+puts "\n"
+puts "compiling ASM files"
 system("cd " + cdir + "/src_asm && make")
 
 puts "\n"
-puts " --------------------------"
-puts " --- Running C Makefile ---"
-puts " --------------------------"
-
+puts "compiling C files"
 system("cd " + cdir + "/src_c && make")
 
 puts "\n"
-puts " -----------------------------"
-puts " --- Running WASM Makefile ---"
-puts " -----------------------------"
-
+puts "compiling WASM files"
 system("cd " + cdir + "/src_wasm && make")
 
 puts "\n"
-puts " ----------------------------"
-puts " --- COPY ASM FINAL FILES ---"
-puts " ----------------------------"
-
+puts "copying compiled ASM files"
 FileUtils.mkdir_p "./" + cdir + "/asm"
 
 Dir.glob("./" + cdir + "/src_asm/*.mem") {|file|
@@ -46,16 +35,28 @@ Dir.glob("./" + cdir + "/src_asm/*.html") {|file|
 	FileUtils.mv(file, "./" + cdir + "/asm")
 }
 
+# Copy the files in /src folders aswell if available
+Dir.glob("./" + cdir + "/src_asm/src/*.mem") {|file|
+	puts "Moving .mem File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/asm")
+}
+
+Dir.glob("./" + cdir + "/src_asm/src/*.js") {|file|
+	puts "Moving .js File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/asm")
+}
+
+Dir.glob("./" + cdir + "/src_asm/src/*.html") {|file|
+	puts "Moving .html File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/asm")
+}
+
 puts "\n"
-puts " --------------------------"
-puts " --- COPY C FINAL FILES ---"
-puts " --------------------------"
+puts "copying compiled ASM files"
 puts "Copying of C Binaries will be skipped"
 
 puts "\n"
-puts " ---------------------------"
-puts " --- COPY JS FINAL FILES ---"
-puts " ---------------------------"
+puts "copying compiled JS files"
 
 FileUtils.mkdir_p "./" + cdir + "/js"
 
@@ -69,10 +70,18 @@ Dir.glob("./" + cdir + "/src_js/*.html") {|file|
 	FileUtils.mv(file, "./" + cdir + "/js")
 }
 
+Dir.glob("./" + cdir + "/src_js/src/*.js") {|file|
+	puts "Moving .js File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/js")
+}
+
+Dir.glob("./" + cdir + "/src_js/src/*.html") {|file|
+	puts "Moving .html File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/js")
+}
+
 puts "\n"
-puts " -----------------------------"
-puts " --- COPY WASM FINAL FILES ---"
-puts " -----------------------------"
+puts "copying compiled WASM files"
 
 FileUtils.mkdir_p "./" + cdir + "/wasm"
 
@@ -91,7 +100,21 @@ Dir.glob("./" + cdir + "/src_wasm/*.html") {|file|
 	FileUtils.mv(file, "./" + cdir + "/wasm")
 }
 
+Dir.glob("./" + cdir + "/src_wasm/src/*.wasm") {|file|
+	puts "Moving .wasm File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/wasm")
+}
+
+Dir.glob("./" + cdir + "/src_wasm/src/*.js") {|file|
+	puts "Moving .js File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/wasm")
+}
+
+Dir.glob("./" + cdir + "/src_wasm/src/*.html") {|file|
+	puts "Moving .html File #{file}"
+	FileUtils.mv(file, "./" + cdir + "/wasm")
+}
+
 puts "\n"
-puts " -----------------"
-puts " --- ALL DONE! ---"
-puts " -----------------"
+puts "finished with " + cdir
+puts "\n"
