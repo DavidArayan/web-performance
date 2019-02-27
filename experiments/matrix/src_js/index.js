e8ae790c2b2de3eee35bd4cdfd211bb292d20b11
David Arayan <david.arayan12@gmail.com>
2019-02-27 17:33:07 +1100

    - Added warmup stage for all JS programs

--- experiments/matrix/src_js/index.js
+++ experiments/matrix/src_js/index.js
@@ -9,7 +9,17 @@ window.addEventListener('DOMContentLoaded', () => {
 			return;
 		}
 
-		new MainEXP().run(parseInt(window.argv));
+		const mainprogc = new MainEXP();
+
+		// warmup
+		console.log("Waming up the program for the JIT");
+		mainprogc.run(10);
+
+		console.log('');
+		console.log('Running Program!');
+		console.log('');
+		// run the actual program
+		mainprogc.run(parseInt(window.argv));
 
 		console.log('Finishing Program!');
 	})();
--- experiments/qsort/src_js/index.js
+++ experiments/qsort/src_js/index.js
@@ -2,14 +2,22 @@ import MainEXP from './main.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 	(function progc() {
-		console.log('Running Program!');
-		
 		if (!window.argv) {
 			console.log('Cannot Run. Supply some arguments!');
 			return;
 		}
 
-		new MainEXP().run(parseInt(window.argv));
+		const mainprogc = new MainEXP();
+
+		// warmup
+		console.log("Waming up the program for the JIT");
+		mainprogc.run(10);
+
+		console.log('');
+		console.log('Running Program!');
+		console.log('');
+		// run the actual program
+		mainprogc.run(parseInt(window.argv));
 
 		console.log('Finishing Program!');
 	})();
