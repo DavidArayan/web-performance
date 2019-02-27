bf102fef8f3322b3b78c7509a4415fbb42afbb52
David Arayan <david.arayan12@gmail.com>
2019-02-27 20:01:25 +1100

    - Added JS implementation of Random and Sort

--- experiments/qsort/src_js/main.js
+++ experiments/qsort/src_js/main.js
@@ -1,7 +1,38 @@
 import RandXOR from './randXOR.js';
 
 export default class MainEXP {
-	run(iterations) {
-		
+	run(numbers) {
+		const rand = new RandXOR(42);
+
+		const values = new Int32Array(numbers);
+
+		let total_time = 0.0;
+
+		let end = Date.now();
+		let begin = Date.now();
+
+		for (var i = 0; i < numbers; i++) {
+			values[i] = rand.nexti(-200000, 200000);
+		}
+
+		end = Date.now();
+
+		total_time = end - begin;
+		console.log('assign random values time ' + total_time);
+
+		const cmp = function(a, b) {
+			return a - b;
+		};
+
+		begin = Date.now();
+
+		values.sort(cmp);
+
+		end = Date.now();
+
+		total_time = end - begin;
+		console.log('sort time ' + total_time);
+
+		return values;
 	}
 }
