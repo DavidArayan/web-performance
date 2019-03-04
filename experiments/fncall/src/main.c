57d4b423793ae191e58c168389de0452670bbffc
David Arayan <david.arayan12@gmail.com>
2019-03-04 15:44:01 +1100

    - Fixed compile issue

--- experiments/fncall/src/main.c
+++ experiments/fncall/src/main.c
@@ -24,7 +24,7 @@
 #endif
 
 // our global variables
-struct random *rand = NULL;
+struct random *global_rand = NULL;
 float *m_model = NULL;
 
 #ifdef __EMSCRIPTEN__
@@ -33,16 +33,19 @@ EMSCRIPTEN_KEEPALIVE
 float calculate_det() {
 	float model_position[3];
 	float model_quaternion[4];
-	float model_scale[] = {100.0f, 100.0f, 100.0f};
+	float model_scale[] = {1.0f, 1.0f, 1.0f};
 
-	const float model_position_x = fmath_random_nextf(rand, -2000.0f, 2000.0f, 100000.0f);
-	const float model_position_y = fmath_random_nextf(rand, -2000.0f, 2000.0f, 100000.0f);
-	const float model_position_z = fmath_random_nextf(rand, -2000.0f, 2000.0f, 100000.0f);
+	const float model_position_x = fmath_random_nextf(global_rand, -200.0f, 200.0f, 100000.0f);
+	const float model_position_y = fmath_random_nextf(global_rand, -200.0f, 200.0f, 100000.0f);
+	const float model_position_z = fmath_random_nextf(global_rand, -200.0f, 200.0f, 100000.0f);
 
-	const float model_rot_x = fmath_random_nextf(rand, 0.0f, 1.0f, 100000.0f);
-	const float model_rot_y = fmath_random_nextf(rand, 0.0f, 1.0f, 100000.0f);
-	const float model_rot_z = fmath_random_nextf(rand, 0.0f, 1.0f, 100000.0f);
-	const float model_rot_w = fmath_random_nextf(rand, 0.0f, 1.0f, 100000.0f);
+	const float model_rot_x = fmath_random_nextf(global_rand, 0.0f, 1.0f, 100000.0f);
+	const float model_rot_y = fmath_random_nextf(global_rand, 0.0f, 1.0f, 100000.0f);
+	const float model_rot_z = fmath_random_nextf(global_rand, 0.0f, 1.0f, 100000.0f);
+	const float model_rot_w = fmath_random_nextf(global_rand, 0.0f, 1.0f, 100000.0f);
+
+	//printf("%f %f %f\n", model_position_x, model_position_y, model_position_z);
+	//printf("%f %f %f %f\n", model_rot_x, model_rot_y, model_rot_z, model_rot_w);
 
 	model_position[0] = model_position_x;
 	model_position[1] = model_position_y;
@@ -72,7 +75,7 @@ EMSCRIPTEN_KEEPALIVE
 void free_buffers() {
 	printf("fncall - free buffers.\n");
 	fmath_matrix4f_free(m_model);
-	fmath_random_free(rand);
+	fmath_random_free(global_rand);
 }
 
 #ifdef __EMSCRIPTEN__
@@ -80,7 +83,7 @@ EMSCRIPTEN_KEEPALIVE
 #endif
 void alloc_buffers() {
 	printf("fncall - allocated buffers.\n");
-	rand = fmath_random_make(42);
+	global_rand = fmath_random_make(4873);
 	m_model = fmath_matrix4f_make();
 }
 
