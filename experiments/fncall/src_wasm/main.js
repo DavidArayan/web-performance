1f3bde64b553797214692c68e318d853f7063f42
David Arayan <david.arayan12@gmail.com>
2019-03-05 17:59:50 +1100

    - Added fncall custom .html/.js files

---
+++ experiments/fncall/src_wasm/main.js
@@ -0,0 +1,30 @@
+export default class MainEXP {
+
+	constructor(global_module) {
+		this.global_module = global_module;
+	}
+
+	run(iterations) {
+		console.log('running iterations ' + iterations);
+		let total_time = 0.0;
+		let value = 0;
+
+		let end = Date.now();
+		let begin = Date.now();
+
+		const global_module = this.global_module;
+
+		for (var i = 0; i < iterations; i++) {
+			value += global_module._calculate_det();
+		}
+
+		end = Date.now();
+
+		total_time = end - begin;
+		console.log('completed time ' + total_time);
+
+		// this gets printed so JIT does not get rid of
+		// our process
+		console.log('final value ' + value);
+	}
+}
